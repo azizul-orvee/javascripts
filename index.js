19c358d6676d1fe0811aeecdfdbdde5f725000d5
Azizul Hakim <mahadi@Mahadis-MacBook-Pro.local>
2022-09-06 02:27:10 +0600

    adsa

--- index.js
+++ index.js
@@ -6,10 +6,4 @@ const data = {
 
 console.log(data)
 
-<<<<<<< HEAD
-#imagine a feature of authentication has been added to this branch.
-
-#this is dev branch, again dev 
-=======
 #this is dev branch.
->>>>>>> dev
